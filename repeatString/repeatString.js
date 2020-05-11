ff785f1060bc0580a1811f1a1a6651ee4a1af01a
Serena <63191000+Sesadada@users.noreply.github.com>
2020-05-11 17:58:05 +0200

    Update repeatString.js

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
 
+const repeatString = (string, n) => {
+  let frase = "";
+while(n > 0) {frase += string; n--}
+  return frase
 }
 
 module.exports = repeatString
+     
+     
+     
+   
