4d7af3498a37b095f3b758a3946d31923ca33000
Serena <63191000+Sesadada@users.noreply.github.com>
2020-05-11 18:28:45 +0200

    Update repeatString.js

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,7 +1,7 @@
 
 const repeatString = function(greeting, num) {
-if (num < 0) return ‘ERROR’
-let str = ’ ';
+if (num < 0) return 'ERROR'
+let str = '';
 for (let i = 0; i < num; i++) {
 str += greeting;
 }
